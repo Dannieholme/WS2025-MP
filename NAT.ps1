8b9f9953e7be2f994ff91a5e01e1ffc7e05acc3b
Dannieholme <118742499+Dannieholme@users.noreply.github.com>
2025-09-25 03:07:33 -0700

    Add files via upload

---
+++ NAT.ps1
@@ -0,0 +1,2 @@
+﻿New-NetNat -name "NAT" -ExternalIPInterfaceAddressPrefix 192.168.103.0/25
+
---
+++ dhcp.ps1
@@ -0,0 +1,8 @@
+﻿#Installation af DHCP
+Install-WindowsFeature -name DHCP -IncludeManagementTools
+#Create Scope
+Add-DhcpServerv4Scope -name "Lan Scope" -StartRange 192.168.103.1 -EndRange 192.168.103.126 -SubnetMask 255.255.255.128
+Add-DhcpServerv4ExclusionRange -ScopeId 192.168.103.0 -StartRange 192.168.103.1 -EndRange 192.168.103.11
+Set-DhcpServerv4OptionValue -ScopeId 192.168.103.0 -OptionId 3 -Value 192.168.103.10
+Set-DhcpServerv4Scope -ScopeId 192.168.103.0 -LeaseDuration ([TimeSpan]::FromDays(1))
+Restart-Computer
