8ed9cfea70b4e9393a8c2fca059db4de16988de5
Dannieholme <118742499+Dannieholme@users.noreply.github.com>
2025-09-25 08:29:07 +0200

    Create Winget.bat

---
+++ Winget.bat
@@ -0,0 +1,17 @@
+echo Installing Google Chrome
+winget install -id Google.Chrome
+
+echo installing Adobe Reader...
+winget install --id Adobe.Acrobat.Reader.64-bit
+
+echo installing Skype...
+winget install --id Microsoft.Skype
+
+echo Installing Java
+winget install --id Oracle.JavaRuntimeEnvironment
+
+echo Installing 7-Zip
+winget install --id 7zip.7zip
+
+echo Installing OpenOffice
+winget install --id Apache.OpenOffice
