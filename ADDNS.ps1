9524222adf31e80155b864c65bf05133e03ab335
Dannieholme <118742499+Dannieholme@users.noreply.github.com>
2025-09-25 12:04:32 +0200

    Update ADDNS.ps1

--- ADDNS.ps1
+++ ADDNS.ps1
@@ -6,5 +6,7 @@ Install-WindowsFeature -Name DNS -IncludeManagementTools
 #Promotion til Domain controller med vigtige indstillinger
 Get-Partition -DiskNumber 0 -PartitionNumber 5 | Set-Partition -NewDriveLetter E
 
+^^^^Kør denne del først, Genstart Computeren og kør denne del vvvv
 
 
+Install-ADDSForest -DomainName "enterprise.$gruppe$user.gf2" -DomainNetbiosName "$gruppe$user" -CreateDnsDelegation:$false -DatabasePath "E:\NTDS" -LogPath "E:\NTDS" -SysvolPath "E:\SYSVOL" -DomainMode "Win2025" -Forestmode "Win2025" -InstallDns:$true -NoRebootOnCompletion:$false -Force:$true
