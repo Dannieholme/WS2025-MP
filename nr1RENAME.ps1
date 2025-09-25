1de301b0235e240658da9521546875d965c89703
Dannieholme <118742499+Dannieholme@users.noreply.github.com>
2025-09-25 04:03:42 -0700

    Update nr1RENAME.ps1

--- nr1RENAME.ps1
+++ nr1RENAME.ps1
@@ -1,6 +1,9 @@
 $group = Read-Host "Hvad er dit gruppe nummer? fx. 03"
 $user = Read-Host "Hvad er dit eget bogstav? fx. C"
 
+#Create Partition til SYSVOL og NTDS
+Get-Partition -DiskNumber 0 -PartitionNumber 5 | Set-Partition -NewDriveLetter E
+
 $newName = "MP$group$user-SRV22"
 
 Rename-Computer -NewName $newName -Force -Restart
