7aebe1db1a5023108a599b2a7bdbe3ddb21a7ec4
Dannieholme <118742499+Dannieholme@users.noreply.github.com>
2025-09-29 11:20:19 +0200

    Update Winget.bat

--- Winget.bat
+++ Winget.bat
@@ -1,20 +1,28 @@
+@echo off
+echo ==================================
 echo Installing Google Chrome
-winget install -id Google.Chrome --silent --accept-package-agreements --accept-source-agreements
-
-echo installing Adobe Reader...
-winget install --id Adobe.Acrobat.Reader.64-bit --silent --accept-package-agreements --accept-source-agreements
-
-echo installing Skype...
-winget install --id Microsoft.Skype --silent --accept-package-agreements --accept-source-agreements
-
-echo Installing Java
-winget install --id Oracle.JavaRuntimeEnvironment --silent --accept-package-agreements --accept-source-agreements
-
+winget install --id Google.Chrome -e --silent --accept-package-agreements --accept-source-agreements
+ 
+echo ==================================
+echo Installing Adobe Reader
+winget install --id Adobe.Acrobat.Reader.64-bit -e --silent --accept-package-agreements --accept-source-agreements
+ 
+echo ==================================
+echo Installing Skype
+winget install --id Skype.Skype -e --silent --accept-package-agreements --accept-source-agreements
+ 
+echo ==================================
+echo Installing Java Runtime (Temurin 17)
+winget install --id Eclipse.Temurin.17.JRE -e --silent --accept-package-agreements --accept-source-agreements
+ 
+echo ==================================
 echo Installing 7-Zip
-winget install --id 7zip.7zip --silent --accept-package-agreements --accept-source-agreements
-
-echo Installing OpenOffice
-winget install --id Apache.OpenOffice --silent --accept-package-agreements --accept-source-agreements
-
+winget install --id 7zip.7zip -e --silent --accept-package-agreements --accept-source-agreements
+ 
+echo ==================================
+echo Installing Apache OpenOffice
+winget install --id Apache.OpenOffice -e --silent --accept-package-agreements --accept-source-agreements
+ 
+echo ==================================
 echo DONE
 pause
