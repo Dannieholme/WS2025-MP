4b469b46b2a274eefa9d0503ef90fec9325a7290
Dannieholme <118742499+Dannieholme@users.noreply.github.com>
2025-09-29 10:33:43 +0200

    Update nr4dhcp.ps1

--- nr4dhcp.ps1
+++ nr4dhcp.ps1
@@ -5,6 +5,8 @@ Add-DhcpServerv4Scope -name "Lan Scope" -StartRange 192.168.103.1 -EndRange 192.
 Add-DhcpServerv4ExclusionRange -ScopeId 192.168.103.0 -StartRange 192.168.103.1 -EndRange 192.168.103.11
 Set-DhcpServerv4OptionValue -ScopeId 192.168.103.0 -OptionId 3 -Value 192.168.103.10
 Set-dhcpserverv4optionvalue -scopeid 192.168.103.0 -OptionId 6 -value 192.168.103.10
+Set-DhcpServerv4OptionValue 15 enterprise.03C.gf2
 Set-DhcpServerv4Scope -ScopeId 192.168.103.0 -LeaseDuration ([TimeSpan]::FromDays(1))
 
 Restart-Computer
+
