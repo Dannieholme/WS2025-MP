c3f7150fec5debaae3640b009af7a7e3dc41dc70
Dannieholme <118742499+Dannieholme@users.noreply.github.com>
2025-09-29 08:56:36 +0200

    Update Winget.bat

--- Winget.bat
+++ Winget.bat
@@ -1,17 +1,20 @@
 echo Installing Google Chrome
-winget install -id Google.Chrome
+winget install -id Google.Chrome --silent --accept-package-agreements --accept-source-agreements
 
 echo installing Adobe Reader...
-winget install --id Adobe.Acrobat.Reader.64-bit
+winget install --id Adobe.Acrobat.Reader.64-bit --silent --accept-package-agreements --accept-source-agreements
 
 echo installing Skype...
-winget install --id Microsoft.Skype
+winget install --id Microsoft.Skype --silent --accept-package-agreements --accept-source-agreements
 
 echo Installing Java
-winget install --id Oracle.JavaRuntimeEnvironment
+winget install --id Oracle.JavaRuntimeEnvironment --silent --accept-package-agreements --accept-source-agreements
 
 echo Installing 7-Zip
-winget install --id 7zip.7zip
+winget install --id 7zip.7zip --silent --accept-package-agreements --accept-source-agreements
 
 echo Installing OpenOffice
-winget install --id Apache.OpenOffice
+winget install --id Apache.OpenOffice --silent --accept-package-agreements --accept-source-agreements
+
+echo DONE
+pause
