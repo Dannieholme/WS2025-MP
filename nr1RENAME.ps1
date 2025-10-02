b15754c3d6cdb9f00a21a79a0d895bde5977e969
Dannieholme <118742499+Dannieholme@users.noreply.github.com>
2025-10-02 05:57:32 +0200

    Update nr1RENAME.ps1

--- nr1RENAME.ps1
+++ nr1RENAME.ps1
@@ -1,9 +1,9 @@
 $group = Read-Host "Hvad er dit gruppe nummer? fx. 03"
 $user = Read-Host "Hvad er dit eget bogstav? fx. C"
 
-#Create Partition til SYSVOL og NTDS
+#Opret Partition til SYSVOL og NTDS
 Get-Partition -DiskNumber 0 -PartitionNumber 5 | Set-Partition -NewDriveLetter E
-
+#Det nye navn vi vil give computeren
 $newName = "MP$group$user-SRV22"
-
+#Giver computeren navnet og genstarter computeren, så alt fungere
 Rename-Computer -NewName $newName -Force -Restart
