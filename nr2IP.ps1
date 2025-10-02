a730f7d57e6a55b2d0d646c8fd98d61c3092e20c
Dannieholme <118742499+Dannieholme@users.noreply.github.com>
2025-10-02 06:02:28 +0200

    Update nr2IP.ps1

--- nr2IP.ps1
+++ nr2IP.ps1
@@ -1,5 +1,6 @@
+#Fjerner alt tidligere configuration af vores LAN(VLAN) adapter
 Remove-NetIPAddress -InterfaceAlias "LAN"
-
+#Laver en ny ip adresse med subnetmaske
 New-NetIPAddress -InterfaceAlias "LAN" -IPAddress 192.168.103.10 -PrefixLength 25
-
+#Sætter DNS adresserne.
 Set-DnsClientServerAddress -InterfaceAlias "LAN" -ServerAddresses ("192.168.103.10","127.0.0.1")
