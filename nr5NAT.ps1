79344e0e745ffe0514f6bbfe936b9ab613791755
Dannieholme <118742499+Dannieholme@users.noreply.github.com>
2025-10-02 10:44:34 +0200

    Update nr5NAT.ps1

--- nr5NAT.ps1
+++ nr5NAT.ps1
@@ -1,5 +1,7 @@
-Opsætter NAT
- New-NetNat -name "NAT" -InternalIPInterfaceAddressPrefix 192.168.103.0/25
+#Opsætter NAT(Network address translation) som giver dine private ip'er en fælles Offentlig IPv4, så vi spare på offentlige IP'er.
+#Desuden skjuler det vores infrastruktur fra verdenen, så vi ikke er lige så nemme at angribe/hacke
+New-NetNat -name "NAT" -InternalIPInterfaceAddressPrefix 192.168.103.0/25
+
 
 
 
