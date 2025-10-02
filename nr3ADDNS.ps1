b845b4698ea7dab2090356024762ab2e85f9d1c3
Dannieholme <118742499+Dannieholme@users.noreply.github.com>
2025-10-02 06:03:47 +0200

    Update nr3ADDNS.ps1

--- nr3ADDNS.ps1
+++ nr3ADDNS.ps1
@@ -1,11 +1,13 @@
-﻿$gruppe = Read-Host "Hvad er dit gruppenummer? fx. 03"
+#Spørger brugeren om Gruppenummer og Bogstav som den så ligger i variaber, som kan bruges når vi laver vores domæne
+ $gruppe = Read-Host "Hvad er dit gruppenummer? fx. 03"
 $user = Read-Host "Hvad er dit bogstav? fx. C"
-#Installation af AD DS og DNS
+#Installation af AD DS og DNS i server manager
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
 Install-WindowsFeature -Name DNS -IncludeManagementTools
-#Promotion til Domain controller med vigtige indstillinger
 
+#Promotion til Domain controller med vigtige indstillinger
 Install-ADDSForest -DomainName "enterprise.$gruppe$user.gf2" -DomainNetbiosName "$gruppe$user" -CreateDnsDelegation:$false -DatabasePath "E:\NTDS" -LogPath "E:\NTDS" -SysvolPath "E:\SYSVOL" -DomainMode "Win2025" -Forestmode "Win2025" -InstallDns:$true -NoRebootOnCompletion:$false -Force:$true
+
 
 
 
